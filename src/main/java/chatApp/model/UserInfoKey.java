685cbad85e55c837cea7daef772cc13d08e81300
momo-0826 <coco.bass.0826@gmail.com>
2023-06-07 22:04:21 +0900

    add @id annotation

--- src/main/java/chatApp/model/UserInfoKey.java
+++ src/main/java/chatApp/model/UserInfoKey.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ import lombok.Data;
 public class UserInfoKey implements Serializable {
 	
 	// id
+	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_info_seq")
     @SequenceGenerator(name = "user_info_seq", sequenceName = "users_id_seq", allocationSize = 1)
 	private Long id;
