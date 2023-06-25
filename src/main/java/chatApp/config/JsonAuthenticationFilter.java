f7b8b153c2a4ac2c6a3fed24cbc790602e9a370c
momo-0826 <coco.bass.0826@gmail.com>
2023-06-25 11:51:05 +0900

    add SecurityConfig

--- src/main/java/chatApp/config/JsonAuthenticationFilter.java
+++ src/main/java/chatApp/config/JsonAuthenticationFilter.java
@@ -1,10 +1,23 @@
 package chatApp.config;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import chatApp.model.UserForm;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class JsonAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	private AuthenticationManager authenticationManager;
@@ -23,16 +36,29 @@ public class JsonAuthenticationFilter extends UsernamePasswordAuthenticationFilt
 		this.setAuthenticationSuccessHandler((req, res, ex) -> {
 			// トークンの生成
 			String token = JWT.create()
-					.withIsuser("com.chatApp") // h発行者
+					.withIssuer("com.chatApp") // h発行者
 					.withClaim("username", ex.getName()) // keyに対してvalueの設定。汎用的な様々な値を保持できる
-					.sign(Algorithm.HMA256("secret")); // 利用アルゴリズムを指定してJWTを新規作成
+					.sign(Algorithm.HMAC256("secret")); // 利用アルゴリズムを指定してJWTを新規作成
 			res.setHeader("X-AUTH-TOKEN", token); // tokeをX-AUTH-TOKENというKeyにセットする
 			res.setStatus(200);
 		});
 		
 		// ログイン失敗時
 		this.setAuthenticationFailureHandler((req,res,ex) -> {
-			req.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		})
+			res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		});
+	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+		try {
+			ServletInputStream stream = request.getInputStream();
+			// リクエストのjsonの値をUserFormにマッピング
+			UserForm form = new ObjectMapper().readValue(request.getInputStream(), UserForm.class);
+			// デフォルトのProviderを利用しつつ、ユーザーレコードの取得に関してはUserDetailsServiceの実装クラスのloadUserByUsernameを利用する
+			return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(form.getUserName(), form.getPassword(), new ArrayList<>()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
---
+++ src/main/java/chatApp/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package chatApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	// ログイン後/homeに遷移させる
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers(header -> {
+			header.frameOptions().disable();
+		});
+		// antMatchers()、mvcMatchers()はSpring security5.8にて削除
+		// requestMatchers()を使用する
+		// "/h2-console/**"以下は、認可を必要とせず、対象パスへのアクセスを許可する。
+		http.authorizeHttpRequests(authorize -> {
+			authorize.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated();
+		});
+		http.formLogin(form -> {
+			form.defaultSuccessUrl("/home");
+		});
+		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withUsername("user")
+				.password(
+						PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("test")
+				).roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
