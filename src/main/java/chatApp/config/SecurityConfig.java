36373fbc5f7357b0cbb3d719b27b61f71f110484
momo-0826 <coco.bass.0826@gmail.com>
2023-06-29 22:29:41 +0900

    add some cors config(testing)

--- src/main/java/chatApp/config/SecurityConfig.java
+++ src/main/java/chatApp/config/SecurityConfig.java
@@ -3,18 +3,23 @@ package chatApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	// ログイン後/homeに遷移させる
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.cors().disable();
+        http.cors().disable();
 		http.csrf().disable();
 		http.headers(header -> {
 			header.frameOptions().disable();
@@ -29,6 +34,19 @@ public class SecurityConfig {
 			form.defaultSuccessUrl("/home");
 		});
 		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+		corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+		corsConfiguration.addExposedHeader("X-AUTH-TOKEN");
+		corsConfiguration.addAllowedOrigin("http://localhost:8080");
+		corsConfiguration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource corsSource =new UrlBasedCorsConfigurationSource();
+		corsSource.registerCorsConfiguration("/**", corsConfiguration);
+		return corsSource;
 	}
 	
 	@Bean
