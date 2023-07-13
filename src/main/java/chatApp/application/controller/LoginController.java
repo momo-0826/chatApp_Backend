e59d7a22e4866f592f8a910aa216cd924d7b925b
momo-0826 <coco.bass.0826@gmail.com>
2023-07-13 09:22:39 +0900

    init spring security

--- src/main/java/chatApp/application/controller/LoginController.java
+++ src/main/java/chatApp/application/controller/LoginController.java
@@ -1,17 +1,16 @@
 package chatApp.application.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 @CrossOrigin(origins = "http://localhost:8080")
 public class LoginController {
 	
-	@GetMapping("login")
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
--- src/main/java/chatApp/config/SecurityConfig.java
+++ src/main/java/chatApp/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,7 +21,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @EnableMethodSecurity
 public class SecurityConfig {
 	
@@ -28,73 +30,20 @@ public class SecurityConfig {
 	
 	private AuthenticationManager authenticationManager;
 	
-	// ログイン後/homeに遷移させる
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().disable();
-//		http.csrf().disable();
-//		http.headers(header -> {
-//			header.frameOptions().disable();
-//		});
-//		// antMatchers()、mvcMatchers()はSpring security5.8にて削除
-//		// requestMatchers()を使用する
-//		http.authorizeHttpRequests(authorize -> {
-//			authorize.requestMatchers("/api/chatApp").permitAll().anyRequest().authenticated();
-//		});
-//		http.formLogin(form -> {
-//			form.defaultSuccessUrl("/home");
-//		});
-//		.addFilterAfter(new RedirectLoginUserFilter(),UsernamePasswordAuthenticationFilter.class);
-		
-		// ラムダDSLで記述
-//		http.formLogin(login -> login
-//				.loginProcessingUrl("/login/process")      // ユーザ名・パスワードの送信先
-//				.loginPage("/login")               // ログイン画面のURL 
-//				.usernameParameter("username")
-//				.passwordParameter("password")
-//				.defaultSuccessUrl("/chat")            // ログイン成功後のリダイレクト先URL
-//				.failureForwardUrl("/login?error") // ログイン失敗後のリダイレクト先URL
-//				.permitAll()                       // ログイン画面については未ログインでもアクセスできるようにする
-//		).logout(logout -> logout
-//				.logoutSuccessUrl("/")             // ログアウト後のリダイレクト先のURL
-//		).authorizeHttpRequests(authz -> authz
-//				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() //"/css/**"等はログイン無しでアクセス可能とする
-//				.requestMatchers("/").permitAll()  // "/"はログイン無しでもアクセス可能とする
-//				.requestMatchers("/general").hasRole("GENERAL")
-//				.requestMatchers("/admin").hasRole("ADMIN")
-//				.requestMatchers("/api/chatApp").permitAll().anyRequest().authenticated()
-//		)
-//		.cors(cors -> cors.disable())
-//		.csrf(csrf -> csrf.disable());
-		
-		// 最小構成で試す
 		http
-        .formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
+		.authorizeHttpRequests((requests) -> requests
+			.requestMatchers("/", "/home", "/login").permitAll()
+			.anyRequest().authenticated()
 		)
-        .logout((logout) -> logout.permitAll())
-        .authorizeHttpRequests(authz -> authz
-//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-        		.requestMatchers("/", "/home", "/api/chatapp/login")
-                .permitAll()
-                .anyRequest().authenticated()
-        )
-        .csrf(csrf -> csrf.disable())
-//        .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
-//        .addFilterAfter(new LoginUserFilter(),UsernamePasswordAuthenticationFilter.class);
-//        .addFilter(new JsonAuthenticationFilter(authenticationManager))
-//		.addFilterAfter(new LoginFilter(),JsonAuthenticationFilter.class)
-		;
-		
-//		http
-//		    .authorizeHttpRequests(requests -> requests
-//		    		.requestMatchers("/", "/home").permitAll()
-//		    		.anyRequest().authenticated()
-//		    ).formLogin(form -> form
-//		    		.loginPage("/login")
-//		    		.permitAll()
-//		    );
+		.formLogin((form) -> form
+//			.loginPage("/login")
+			.permitAll()
+		)
+		.logout((logout) -> logout.permitAll()
+		)
+		.csrf((csrf) -> csrf.disable());
 		return http.build();
 	}
 	
@@ -125,6 +74,15 @@ public class SecurityConfig {
 		System.out.println(user.getPassword());
 		return new InMemoryUserDetailsManager(user);
 	}
+
+	/**
+     * パスワードをBCryptで暗号化するクラス
+     * @return パスワードをBCryptで暗号化するクラスオブジェクト
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 	
 //	@Bean
 //	public UserDetailsManager userDetailsService() {
